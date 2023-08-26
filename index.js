16c1508e4d36f3971f16dd286820a6cce3e92b3d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-26 13:55:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
 
 addButton.addEventListener("click", addTask);
+clearAllButton.addEventListener("click", clearAllTasks);
