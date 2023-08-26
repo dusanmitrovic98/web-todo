820ca2f9eb8fcef947cce27e13012fa46ea8785a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-26 13:55:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const addButton = document.getElementById("add-button");
 const clearAllButton = document.getElementById("clear-all-button");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
+
